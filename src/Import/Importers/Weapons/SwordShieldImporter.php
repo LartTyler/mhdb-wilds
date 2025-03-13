bd1bf633e25f6842a7c12b9a52f7dfb9bc88e80e
Tyler Lartonoix <tyler@lartonoix.com>
2025-03-13 02:59:50 -0400

    Add missing AsImporter attribute

--- src/Import/Importers/Weapons/SwordShieldImporter.php
+++ src/Import/Importers/Weapons/SwordShieldImporter.php
@@ -2,8 +2,10 @@
 	namespace App\Import\Importers\Weapons;
 
 	use App\Entity\Weapons\SwordShield;
+	use App\Import\AsImporter;
 	use App\Import\ImportContext;
 
+	#[AsImporter]
 	class SwordShieldImporter extends AbstractWeaponImporter implements SharpnessImporterInterface {
 		public function __invoke(ImportContext $context): void {
 			$this->run($context, 'SwordShield', SwordShield::class);
